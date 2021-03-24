98f56c1a1376ac4edb4018f6c872a7f24109ba46
142434 <utkars.rajput@gmail.com>
2021-03-24 17:57:09 +0530

    ww2

--- hts/lib/3pre.js
+++ hts/lib/3pre.js
@@ -1,5 +1,6 @@
-Module['onAbort']=function(what){console.log(what);postMessage('reload');}
-Module['locateFile']=function(path,prefix){
+var Module={'onAbort':function(what){console.log(what);postMessage('reload');},
+'locateFile':function(path,prefix){
 	console.log(prefix+' -> '+path)
 	return "https://031323.github.io/suvak/"+path;
+}
 }
