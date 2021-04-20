1ea3b1459f82acef48a9e07726a3865a0cad41e2
142434 <utkars.rajput@gmail.com>
2021-04-20 18:36:48 +0530

    aw

--- suvak2.js
+++ suvak2.js
@@ -1,8 +1,35 @@
 var context = new AudioContext({sampleRate:48000,latencyHint:"playback"});
 
-suvagarmbh=function(prtikrm) {
+var website;
+if(location.href.includes('localhost'))
+	website='https://'+location.host+'/';
+else
+	website='https://031323.github.io/suvak/';
+
+suvagarmbh=function(prtikrm,reload) {
+	var xhr = new XMLHttpRequest();
+	//xhr.open('GET', 'http://0.0.0.0:8000/suvakww.js');
+	//xhr.open('GET', 'http://localhost:8080/suvakww.js');
+	xhr.open('GET', website+'suvakworker.js');
+	xhr.onload = function() {
+    if (xhr.status === 200) {
+        var workerSrcBlob, workerBlobURL;
+        
+            workerSrcBlob = new Blob([xhr.responseText], { type: 'text/javascript' });
+            workerBlobURL = window.URL.createObjectURL(workerSrcBlob);
+		context.audioWorklet.addModule(workerBlobURL).then(() => { prtikrm() });
+    }
+		else reload();
+};
+xhr.onerror=reload;
+xhr.send();
+}
+
+suvagarmbh=function(prtikrm,reload) {
+	
 	context.audioWorklet.addModule('suvakworker.js').then(() => { prtikrm() });
 }
+
 
 suvacnarmbh=function(vakym,prtikrm)
 {
