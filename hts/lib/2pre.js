0f0bd678d8a754b2b8493231286d04f837442e3a
142434 <utkars.rajput@gmail.com>
2021-04-22 10:55:49 +0530

    reload

--- hts/lib/2pre.js
+++ hts/lib/2pre.js
@@ -1,2 +1,10 @@
+//var website='http://localhost:8000/';
+
 var Module={'onAbort':function(what){console.log(what);postMessage('reload');}
+/*,'locateFile':function(path,prefix){
+	console.log(prefix+' -> '+path)
+	//return "http://0.0.0.0:8000/"+path;
+	//return "http://localhost:8080/"+path;
+	return website+path;
+}*/
 }
