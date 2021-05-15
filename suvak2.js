07dacb391c56d0ee43a1b405a808264d06913b9f
142434 <utkars.rajput@gmail.com>
2021-05-15 12:05:04 +0530

    svritsvrh

--- suvak2.js
+++ suvak2.js
@@ -1,4 +1,4 @@
-var suvaggaurvm=4788078;
+var suvaggaurvm=4788655;
 
 var context = new AudioContext({sampleRate:48000,latencyHint:"playback"});
 
@@ -70,6 +70,15 @@ suvacnarmbh=function(vakym,armbkrm,prtikrm)
     	suvak_reload();
     }
   };
-  node.port.postMessage([vakym,Math.log(suvak_svr0),Math.log(suvak_svr1)]);
+  let sa=suvak_svr0;
+  let su=suvak_svr1;
+  let snn=suvak_svr0;
+  if(suvak_svritanudatth>0)sa=suvak_svritanudatth;
+  if(suvak_svritodatth>0)su=suvak_svritodatth;
+  if(suvak_udattpurvanudatth>0)snn=suvak_udattpurvanudatth;
+  node.port.postMessage([vakym,Math.log(suvak_svr0),Math.log(suvak_svr1),Math.log(sa),Math.log(su),Math.log(snn)]);
 }
 var suvak_svr0=100,suvak_svr1=135;
+var suvak_svritanudatth=-1;
+var suvak_svritodatth=-1;
+var suvak_udattpurvanudatth=-1;
